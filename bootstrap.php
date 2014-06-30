069887af52dea5180bd52d3fb9f1e60964140212
BlackScorp <witalimik@web.de>
2014-06-30 15:20:14 +0200

    added pdo to DI container

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,19 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use Logd\Core\Env;
 
-if(!Env::isTest()){
-require 'dbconnect.php';
-$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8',$DB_HOST,$DB_NAME);
-$pdo = new PDO($dsn,$DB_USER,$DB_PASS);
-$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+if (!Env::isTest()) {
+    require 'dbconnect.php';
+
+    $app = new Pimple\Container();
+
+    $app['db'] = function ($app) use ($DB_HOST, $DB_NAME,$DB_USER, $DB_PASS) {
+        $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $DB_HOST, $DB_NAME);
+        $pdo = new PDO($dsn, $DB_USER, $DB_PASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    };
+    $app['prefix'] = $DB_PREFIX;
+
 }
