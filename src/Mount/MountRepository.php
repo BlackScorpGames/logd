45a2da878e4eab2426adabab72ab670c52e7057e
BibaltiK <BibaltiK@exdrals.de>
2019-12-18 03:52:07 +0100

    add CRUD Methods

--- src/Mount/MountRepository.php
+++ src/Mount/MountRepository.php
@@ -21,6 +21,116 @@ class MountRepository implements MountRepositoryInteface
         $this->dbPrefix = $dbPrefix;
     }
     
+    public function create(MountEntity $mount) 
+    {
+        $sql = ('INSERT INTO '
+                    .'`'. $this->dbPrefix . 'mounts` '
+                    . '('
+                        . '`mountname`, '
+                        . '`mountdesc`, '
+                        . '`mountcategory`, '
+                        . '`mountbuff`, '
+                        . '`mountcostgems`, '
+                        . '`mountcostgold`, '
+                        . '`mountactive`, '
+                        . '`mountforestfights`, '
+                        . '`newday`, '
+                        . '`recharge`, '
+                        . '`partrecharge`, '
+                        . '`mountfeedcost`, '
+                        . '`mountlocation`, '
+                        . '`mountdkcost`'
+                    . ')'
+              . 'VALUES '
+                    . '('
+                        . ':name, '
+                        . ':desc, '
+                        . ':category, '
+                        . ':buff, '
+                        . ':costgems, '
+                        . ':costgold, '
+                        . ':active, '
+                        . ':forestfights, '
+                        . ':newday, '
+                        . ':recharge, '
+                        . ':partrecharge, '
+                        . ':feedcost, '
+                        . ':location, '
+                        . ':dkcost'
+                    . ')');
+        $bindParam = [
+                        ':name'         => $mount->getName(),
+                        ':desc'         => $mount->getDesc(),
+                        ':category'     => $mount->getCategory(),
+                        ':buff'         => $mount->getBuff(),
+                        ':costgems'     => $mount->getCostGems(),
+                        ':costgold'     => $mount->getCostGold(),
+                        ':active'       => (int)$mount->getActive(),
+                        ':forestfights' => $mount->getForestFights(),
+                        ':newday'       => $mount->getNewDay(),
+                        ':recharge'     => $mount->getRecharge(),
+                        ':partrecharge' => $mount->getPartRecharge(),
+                        ':feedcost'     => $mount->getFeedCost(),
+                        ':location'     => $mount->getLocation(),
+                        ':dkcost'       => $mount->getDkCost(),
+                        ':mountID'      => $mount->getID()
+                    ];
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($bindParam);
+    }
+    
+    public function update(MountEntity $mount) 
+    {
+        $sql = ('UPDATE '
+                    .'`'. $this->dbPrefix . 'mounts` '
+              . 'SET '
+                    . '`mountname`=:name, '
+                    . '`mountdesc`=:desc, '
+                    . '`mountcategory`=:category, '
+                    . '`mountbuff`=:buff, '
+                    . '`mountcostgems`=:costgems, '
+                    . '`mountcostgold`=:costgold, '
+                    . '`mountactive`=:active, '
+                    . '`mountforestfights`=:forestfights, '
+                    . '`newday`=:newday, '
+                    . '`recharge`=:recharge, '
+                    . '`partrecharge`=:partrecharge, '
+                    . '`mountfeedcost`=:feedcost, '
+                    . '`mountlocation`=:location, '
+                    . '`mountdkcost`=:dkcost '
+              . 'WHERE '
+                    . '`mountid`=:mountID');        
+        $bindParam = [
+                        ':name'         => $mount->getName(),
+                        ':desc'         => $mount->getDesc(),
+                        ':category'     => $mount->getCategory(),
+                        ':buff'         => $mount->getBuff(),
+                        ':costgems'     => $mount->getCostGems(),
+                        ':costgold'     => $mount->getCostGold(),
+                        ':active'       => (int)$mount->getActive(),
+                        ':forestfights' => $mount->getForestFights(),
+                        ':newday'       => $mount->getNewDay(),
+                        ':recharge'     => $mount->getRecharge(),
+                        ':partrecharge' => $mount->getPartRecharge(),
+                        ':feedcost'     => $mount->getFeedCost(),
+                        ':location'     => $mount->getLocation(),
+                        ':dkcost'       => $mount->getDkCost(),
+                        ':mountID'      => $mount->getID()
+                    ];
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($bindParam);
+    }
+    
+    public function delete(int $id)
+    {
+        $sql = ('DELETE FROM '
+                    .'`'. $this->dbPrefix . 'mounts` '
+              . 'WHERE '
+                    . '`mountid`=:mountID');
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':mountID' => $id]);
+    }
+
     public function findMount(int $mountID=0) : MountEntity 
     {
         $sql = 'SELECT '
@@ -40,9 +150,9 @@ class MountRepository implements MountRepositoryInteface
                 . '`mountlocation`,'
                 . '`mountdkcost`'
             .  'FROM '
-                . $this->dbPrefix . 'mounts'
+                . '`'. $this->dbPrefix . 'mounts` '
             . ' WHERE '
-                . 'mountid=:mountID';
+                . '`mountid`=:mountID';
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindParam(':mountID', $mountID);
         $stmt->execute();
@@ -69,5 +179,6 @@ class MountRepository implements MountRepositoryInteface
 	}*/
 	return array($name, $lcname);
     }
-
+    
+    
 }
